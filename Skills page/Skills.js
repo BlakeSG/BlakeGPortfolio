89014fa1e334f59614ca7a38e4eb1f0b1ba998c9
BlakeSG <139823308+BlakeSG@users.noreply.github.com>
2023-10-18 14:54:24 +1300

    Add files via upload
    skills javascript page has functionality for items on page

---
+++ Skills page/Skills.js
@@ -0,0 +1,42 @@
+// LIGHT DARK MODE
+document.addEventListener("DOMContentLoaded", function() {
+    const Light_Mode = document.getElementById("Lightmode");
+    const Dark_Mode = document.getElementById("Darkmode");
+    const elementToChange = document.getElementById("elementToChange");
+   
+
+
+    function setModeToLocalStorage(mode) {
+        localStorage.setItem("selectedMode", mode);
+    }
+
+
+    function applySelectedMode() {
+        const selectedMode = localStorage.getItem("selectedMode");
+
+        if (selectedMode === "Lightmode") {
+            elementToChange.classList.remove("Darkmode");
+            elementToChange.classList.add("Lightmode");
+          
+        } else if (selectedMode === "Darkmode") {
+            elementToChange.classList.remove("Lightmode");
+            elementToChange.classList.add("Darkmode");
+         
+        }
+    }
+
+   
+    applySelectedMode();
+
+    Light_Mode.addEventListener("change", function() {
+        console.log("Light Mode clicked");
+        setModeToLocalStorage("Lightmode");
+        applySelectedMode();
+    });
+
+    Dark_Mode.addEventListener("change", function() {
+        console.log("Dark Mode clicked");
+        setModeToLocalStorage("Darkmode");
+        applySelectedMode();
+    });
+});
